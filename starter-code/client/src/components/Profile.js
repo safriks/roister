441dfd3b8c56f85ed19c6ab29aaf27e68ce99d56
Julieta <jmartinezrey@hotmail.com>
2019-10-31 11:47:56 +0100

    profile styled

--- starter-code/client/src/components/Profile.js
+++ starter-code/client/src/components/Profile.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import axios from "axios";
-=======
 import instance from "../auth/customAxios";
->>>>>>> 86d2bb20f186df683465590091554f81845070d0
 
 import {
     Button,
@@ -11,10 +7,6 @@ import {
     TabPane,
     Container,
     Row,
-<<<<<<< HEAD
-    Input,
-=======
->>>>>>> 86d2bb20f186df683465590091554f81845070d0
     Col,
     UncontrolledTooltip
   } from "reactstrap";
@@ -25,71 +17,6 @@ import {
 export default class Profile extends Component {
     constructor(props){
         super(props);
-<<<<<<< HEAD
-        this.formRef = React.createRef();
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    state = {
-            firstname: '',
-            lastname: '',
-            username: '',
-            location: '',
-            aboutme: '',
-            skills: '',
-            jobposition: '',
-        }
-
-handleChange(e){
-    this.setState({
-        [e.target.name]: e.target.value
-    })
-}
-
-handleSubmit(e) {
-    e.preventDefault(); 
-    let user = JSON.parse(localStorage.getItem('user'));
-    let formData = new FormData(this.formRef.current); 
-    axios({
-        url: "https://localhost:3001/createprofile",
-        data: this.state,
-        data1: formData,
-            headers: {
-                'content-type': 'multipart/form-data'
-            },     
-        method: "POST"
-    })
-    .then((response)=> {
-        this.props.history.push(`/profile/${response.data._id}`)
-    })
-    .catch((error)=> {
-        
-    })
-    
-}
-
-render(){
-    return(           
-    <>
-    <ExamplesNavbar />
-    <div className="wrapper">
-    <div
-        className="page-header clear-filter page-header-small"
-        filter-color="blue"
-      >
-        <div
-          className="page-header-image"
-          style={{
-            backgroundImage: "url(" + require("assets/img/alexander.jpg") + ")"
-          }}       
-        ></div>
-        <Container>
-          <div >
-          <img className="photo-container" alt="..." src={require("assets/img/icons-profile.png")}></img>
-          </div>
-          <h4 className="title">Welcome {this.state.username}!</h4>      
-=======
     }
 
     state = {
@@ -127,36 +54,36 @@ render(){
           
         ></div>
         <Container>
-          <div >
-
+          <div>
             <img className="photo-container" alt="..." src={this.state.user.picture}></img>
           </div>
-          <h4 className="title">Welcome {this.state.user.username}!</h4>
-          <h5 className="title">Create your profile</h5>
->>>>>>> 86d2bb20f186df683465590091554f81845070d0
+          <h2 className="title">Welcome {this.state.user.firstname}</h2>
+          <h3>{this.state.user.jobposition}</h3>
         </Container>
       </div>
       <div className="section">
         <Container>
           <div className="button-container">
-            <Button className="btn-round" color="info" size="lg">
-<<<<<<< HEAD
-              Connect 
-=======
-              Follow
->>>>>>> 86d2bb20f186df683465590091554f81845070d0
+            <Button color='primary' className="btn-round" size="lg">
+              Connect
             </Button>
+            <div class="left">   
+            <button class="social-signin messages">Messages</button>
+            <button class="social-signin messages">Settings</button>
+            <button class="social-signin messages">Edit Profile</button>
+          </div>
+
+           <div class="right">   
+            <button class="social-signin facebook">Follow me on Facebook</button>
+            <button class="social-signin twitter">Follow me on Twitter</button>
+            <button class="social-signin google">Follow me on Google+</button>
+          </div>
             <Button
               className="btn-round btn-icon"
               color="default"
               id="tooltip515203352"
               size="lg"
             >
-<<<<<<< HEAD
-            <i className="fab fa-twitter"></i>
-=======
-              <i className="fab fa-twitter"></i>
->>>>>>> 86d2bb20f186df683465590091554f81845070d0
             </Button>
             <UncontrolledTooltip delay={0} target="tooltip515203352">
               Follow me on Twitter
@@ -166,47 +93,24 @@ render(){
               color="default"
               id="tooltip340339231"
               size="lg"
-            >
-<<<<<<< HEAD
-            <i className="fab fa-instagram"></i>
-=======
-              <i className="fab fa-instagram"></i>
->>>>>>> 86d2bb20f186df683465590091554f81845070d0
+            >          
             </Button>
             <UncontrolledTooltip delay={0} target="tooltip340339231">
               Follow me on Instagram
             </UncontrolledTooltip>
-<<<<<<< HEAD
-          </div>  
-          <h3 className="title">Location</h3>        
-          <h3 className="title">Skills</h3>        
-          <h3 className="title">About me</h3>         
-          <h3 className="title">Job Position</h3>
-          <Button className="btn-round">
-                  <Input 
-                    onChange={(e) => this.handleInputUpload(e)}  
-                    placeholder="Upload a picture"  
-                    type="file" 
-                    required name="picture"
-                  ></Input>
-                  </Button>         
-=======
+          </div>        
+
+          <div className='profile-details-container'>
+          <h4 className='title text-center'>Location</h4>
+          <h4>{this.state.user.location}</h4>
+          <h4 className='title text-center'>Skills</h4>
+          <h4>{this.state.user.skills}</h4>
+          <h4 className='title text-center'>About me</h4>
+          <h4>{this.state.user.aboutme}</h4>
           </div>
-          
-          <h3 className="title">Location</h3>
-         
-          <h3 className="title">Skills</h3>
-          
-          <h3 className="title">About me</h3>
-          
-          <h3 className="title">Job Position</h3>
-          
-          <h3 className="title">Degree</h3>
-          
->>>>>>> 86d2bb20f186df683465590091554f81845070d0
           <Row>
             <Col className="ml-auto mr-auto" md="6">
-              <h4 className="title text-center">My Portfolio</h4>
+              <h4 className="title text-center">My Projects</h4>
               <div className="nav-align-center">
                 <Nav
                   className="nav-pills-info nav-pills-just-icons"
@@ -243,10 +147,7 @@ render(){
                 </Col>
               </TabPane>
           </Row>
-<<<<<<< HEAD
-=======
           <br></br>
->>>>>>> 86d2bb20f186df683465590091554f81845070d0
         </Container>
       </div>
       <DefaultFooter/>
